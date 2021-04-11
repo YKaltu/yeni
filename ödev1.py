7eff1d1a68b5826235c97652db75619ae2830a51
Yeşim Kaltu <80278300+YKaltu@users.noreply.github.com>
2021-04-12 00:00:47 +0300

    Add files via upload

---
+++ ödev1.py
@@ -0,0 +1,25 @@
+#ödev1:1
+
+liste = [1, 2, 3, 4, 5, 6]
+
+liste1 = liste[ 0:3:1 ]
+
+liste2 = liste[ 3:6:1 ]
+
+
+
+liste1 = [1, 2, 3]
+
+liste2 = [4, 5, 6]
+                
+yliste= liste2+liste1
+
+print(yliste)
+                
+
+#ödev1:2
+number = int(input("Please enter a single digit number from 0 to 4: "))
+
+for i in range(0,4,2):
+    print(i)
+
